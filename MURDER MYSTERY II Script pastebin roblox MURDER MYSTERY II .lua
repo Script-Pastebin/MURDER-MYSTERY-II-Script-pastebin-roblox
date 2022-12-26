c3ac49a07ed677f3d9d20227c07c7f7f172b5aff
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:59:45 +0100

    commit zero

---
+++ MURDER MYSTERY II Script pastebin roblox MURDER MYSTERY II .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/XXuX12Nt"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Very cool script for MURDER MYSTERY II script is very functional :)
+						
+]]
